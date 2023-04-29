1c20955b56f8f480ac95089c6a51a9fced85b4c2
mdyasinahmed <yasinahmed7875@gmail.com>
2023-04-30 00:37:06 +0600

    python variable learning code file added

---
+++ variables.py
@@ -0,0 +1,26 @@
+# assign value to uni_name variable
+uni_name = 'metro.uni'
+print(uni_name)
+
+# changing value
+uni_name = 'metropolitan.uni'
+print(uni_name)
+
+# Assigning multiple values to multiple variables
+a, b, c = 10, 20.5, 'Hello'
+print(a) #print 10
+print(b) #print 20.5
+print(c) #print hello
+    # same value to multiple variables
+
+name1 = name2 = 'Metropolitan'
+print(name1)
+print(name2)
+
+'''
+*Standard Python Variable Naming:
+snake_case
+MACRO_CASE
+camelCase
+CapWords
+'''
