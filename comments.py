41b4bd9b28a0dba6cdb94c4bf54afc37e4d0aa0c
mdyasinahmed <yasinahmed7875@gmail.com>
2023-06-14 22:36:29 +0600

    sum.py is added

--- comments.py
+++ comments.py
@@ -5,3 +5,12 @@ result=num1+num2
 #print the output
 print('This is output')
 print(result)
+'''This is long comment
+a a perfect example of
+multi-line comment'''
+
+#use of Python Comment
+'''
+    1. Making code easier to understand
+    2. Using comment for Debugging
+'''
