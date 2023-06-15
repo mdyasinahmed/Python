4338744866e23e8768df3c4a30d9e4943e795582
mdyasinahmed <yasinahmed7875@gmail.com>
2023-06-15 23:23:35 +0600

    more python code added

---
+++ type-conversion.py
@@ -0,0 +1,10 @@
+#python takes all the input as a string
+old_age = input("enter your old age: ")
+new_age = int(old_age) + 2
+print(new_age)
+
+number = float(18)
+print(number)
+
+str(number)
+print(number)
