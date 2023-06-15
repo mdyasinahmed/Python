98fd3094253c4659630226a5de5c0ee1a8c4890d
mdyasinahmed <yasinahmed7875@gmail.com>
2023-06-15 23:55:15 +0600

    ArithmeticOperators code is added

---
+++ ArithmeticOperators.py
@@ -0,0 +1,20 @@
+# + operator
+print(6 + 4)
+
+# - operator
+print(6 - 4)
+
+# * operator
+print(6 * 4)
+
+# / operator
+print(6 / 4)
+
+# // operator
+print(6 // 4)
+
+# % operator
+print(6 % 4)
+
+# ** operator
+print(3 ** 3) #3^3
