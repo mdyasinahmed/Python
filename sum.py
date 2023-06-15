130c46f7ae1786d8bb96514c2ce412ebd7e9fad4
mdyasinahmed <yasinahmed7875@gmail.com>
2023-06-15 23:34:42 +0600

    type conversion more code added

---
+++ sum.py
@@ -0,0 +1,25 @@
+''' 
+
+first = int(input("enter first number: "))
+second = int(input("enter second number: "))
+
+sum = first + second
+print(sum)
+
+'''
+
+'''
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+sum = int(a) + int(b)
+print(sum)
+'''
+
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+sum = int(a) + int(b)
+print("the sum is : " + str(sum))
+
+
