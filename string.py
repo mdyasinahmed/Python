7f360cb2dd367226b07af04ae9a720eb4ea00fb6
mdyasinahmed <yasinahmed7875@gmail.com>
2023-06-15 23:48:45 +0600

    string in python code is added

---
+++ string.py
@@ -0,0 +1,15 @@
+name = "Metropolitan University"
+
+
+# print(name.upper())
+
+# print(name.lower())
+
+
+#print(name.find('s'))
+
+
+# print(name.replace("Metropolitan", "Toronto"))
+
+print("t" in name)
+print("T" in name)
