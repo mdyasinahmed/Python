b5a72ab9ad23b100634554309124e4aa4ca4e882
mdyasinahmed <yasinahmed7875@gmail.com>
2023-06-24 15:40:18 +0600

    python function arguments practice code added

---
+++ intro-project/RPS - Functions Arguments copy.py
@@ -0,0 +1,23 @@
+# libraries
+# import random
+
+
+# lists
+# food = ["biriyani", "polaw", "kichuri", "kabab", "halim"]
+# print(food)
+
+import random                                                           #libraries
+def get_choices():                                                      #user defined function
+    player_choice = input("Enter a Choice(rock/paper/scissors): ")      #taking input
+    options = ["rock", "paper", "scissors"]                             #lists
+    computer_choices = random.choice(options)                           #library methods
+    choices = {"Player": player_choice, "Computer": computer_choices}   #dictionaries
+
+    return choices                  
+
+# choices = get_choices()            #calling function                                    
+# print(choices)                     #printing through calling function in variable    
+
+# Function Arguments
+def check_win(player, computer):
+    return [player, computer]
