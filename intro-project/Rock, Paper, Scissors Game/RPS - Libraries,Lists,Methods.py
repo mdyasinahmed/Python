6f2eb7ddd1920e30fd928c76f0868e332d6915c1
mdyasinahmed <yasinahmed7875@gmail.com>
2023-06-24 15:14:25 +0600

    python learning intro projects code added

---
+++ intro-project/Rock, Paper, Scissors Game/RPS - Libraries,Lists,Methods.py
@@ -0,0 +1,32 @@
+# Variables
+player_choice = "rock"
+computer_choice = "paper"
+
+# Functions
+def get_choices():
+    player_choice = "rock"
+    computer_choice = "paper"
+
+    return computer_choice
+
+def greetings():
+    return "Hi"
+
+# call a function
+response = greetings()
+print(response)
+
+# choices = get_choices()
+# print(choices)
+
+# others way
+# print(greetings())
+print(get_choices())
+
+
+# dictionaries
+
+dict = {"name":"Yasin", "age":20}      # name_of_dict = {key, key, ...}
+
+take_it = input("Enter a Choice: ")
+print(take_it)
---
+++ intro-project/Rock, Paper, Scissors Game/RPS - Variables_and_Functions copy.py
@@ -0,0 +1,24 @@
+# Variables
+player_choice = "rock"
+computer_choice = "paper"
+
+# Functions
+def get_choices():
+    player_choice = "rock"
+    computer_choice = "paper"
+
+    return computer_choice
+
+def greetings():
+    return "Hi"
+
+# call a function
+response = greetings()
+print(response)
+
+# choices = get_choices()
+# print(choices)
+
+# others way
+# print(greetings())
+print(get_choices())
